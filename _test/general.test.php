5d40778c341f604b30b43351ad1d6f602965a85f
Felix Haase <felix.haase@feki.de>
2020-09-20 03:31:46 +0200

    formatting

--- _test/general.test.php
+++ _test/general.test.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * General tests for the ldapsearch plugin
  *
@@ -8,69 +9,68 @@
 class general_plugin_ldapsearch_test extends DokuWikiTest
 {
 
-    /**
-     * Simple test to make sure the plugin.info.txt is in correct format
-     */
-    public function test_plugininfo()
-    {
-        $file = __DIR__ . '/../plugin.info.txt';
-        $this->assertFileExists($file);
+	/**
+	 * Simple test to make sure the plugin.info.txt is in correct format
+	 */
+	public function test_plugininfo()
+	{
+		$file = __DIR__ . '/../plugin.info.txt';
+		$this->assertFileExists($file);
 
-        $info = confToHash($file);
+		$info = confToHash($file);
 
-        $this->assertArrayHasKey('base', $info);
-        $this->assertArrayHasKey('author', $info);
-        $this->assertArrayHasKey('email', $info);
-        $this->assertArrayHasKey('date', $info);
-        $this->assertArrayHasKey('name', $info);
-        $this->assertArrayHasKey('desc', $info);
-        $this->assertArrayHasKey('url', $info);
+		$this->assertArrayHasKey('base', $info);
+		$this->assertArrayHasKey('author', $info);
+		$this->assertArrayHasKey('email', $info);
+		$this->assertArrayHasKey('date', $info);
+		$this->assertArrayHasKey('name', $info);
+		$this->assertArrayHasKey('desc', $info);
+		$this->assertArrayHasKey('url', $info);
 
-        $this->assertEquals('ldapsearch', $info['base']);
-        $this->assertRegExp('/^https?:\/\//', $info['url']);
-        $this->assertTrue(mail_isvalid($info['email']));
-        $this->assertRegExp('/^\d\d\d\d-\d\d-\d\d$/', $info['date']);
-        $this->assertTrue(false !== strtotime($info['date']));
-    }
+		$this->assertEquals('ldapsearch', $info['base']);
+		$this->assertRegExp('/^https?:\/\//', $info['url']);
+		$this->assertTrue(mail_isvalid($info['email']));
+		$this->assertRegExp('/^\d\d\d\d-\d\d-\d\d$/', $info['date']);
+		$this->assertTrue(false !== strtotime($info['date']));
+	}
 
-    /**
-     * Test to ensure that every conf['...'] entry in conf/default.php has a corresponding meta['...'] entry in
-     * conf/metadata.php.
-     */
-    public function test_plugin_conf()
-    {
-        $conf_file = __DIR__ . '/../conf/default.php';
-        if (file_exists($conf_file)) {
-            include($conf_file);
-        }
-        $meta_file = __DIR__ . '/../conf/metadata.php';
-        if (file_exists($meta_file)) {
-            include($meta_file);
-        }
+	/**
+	 * Test to ensure that every conf['...'] entry in conf/default.php has a corresponding meta['...'] entry in
+	 * conf/metadata.php.
+	 */
+	public function test_plugin_conf()
+	{
+		$conf_file = __DIR__ . '/../conf/default.php';
+		if (file_exists($conf_file)) {
+			include $conf_file;
+		}
+		$meta_file = __DIR__ . '/../conf/metadata.php';
+		if (file_exists($meta_file)) {
+			include $meta_file;
+		}
 
-        $this->assertEquals(
-            gettype($conf),
-            gettype($meta),
-            'Both ' . DOKU_PLUGIN . 'ldapsearch/conf/default.php and ' . DOKU_PLUGIN . 'ldapsearch/conf/metadata.php have to exist and contain the same keys.'
-        );
+		$this->assertEquals(
+			gettype($conf),
+			gettype($meta),
+			'Both ' . DOKU_PLUGIN . 'ldapsearch/conf/default.php and ' . DOKU_PLUGIN . 'ldapsearch/conf/metadata.php have to exist and contain the same keys.'
+		);
 
-        if (gettype($conf) != 'NULL' && gettype($meta) != 'NULL') {
-            foreach ($conf as $key => $value) {
-                $this->assertArrayHasKey(
-                    $key,
-                    $meta,
-                    'Key $meta[\'' . $key . '\'] missing in ' . DOKU_PLUGIN . 'ldapsearch/conf/metadata.php'
-                );
-            }
+		if (gettype($conf) != 'NULL' && gettype($meta) != 'NULL') {
+			foreach ($conf as $key => $value) {
+				$this->assertArrayHasKey(
+					$key,
+					$meta,
+					'Key $meta[\'' . $key . '\'] missing in ' . DOKU_PLUGIN . 'ldapsearch/conf/metadata.php'
+				);
+			}
 
-            foreach ($meta as $key => $value) {
-                $this->assertArrayHasKey(
-                    $key,
-                    $conf,
-                    'Key $conf[\'' . $key . '\'] missing in ' . DOKU_PLUGIN . 'ldapsearch/conf/default.php'
-                );
-            }
-        }
-
-    }
+			foreach ($meta as $key => $value) {
+				$this->assertArrayHasKey(
+					$key,
+					$conf,
+					'Key $conf[\'' . $key . '\'] missing in ' . DOKU_PLUGIN . 'ldapsearch/conf/default.php'
+				);
+			}
+		}
+	}
 }
