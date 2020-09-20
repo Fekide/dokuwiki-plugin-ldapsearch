426605292c36724ebed9ccc6637d55821209fec0
Felix Haase <felix.haase@feki.de>
2020-09-20 03:57:30 +0200

    Remove fieldsets

--- conf/metadata.php
+++ conf/metadata.php
@@ -5,9 +5,7 @@
  * @author Felix Haase <felix.haase@feki.de>
  */
 
-$meta['_global'] = array('fieldset');
 $meta['allow_overrides'] = array('onoff');
-$meta['_hosts'] = array('fieldset');
 $meta['name'] = array('string', '_pattern' => '/^(\w+\|?)*$/'); 
 $meta['hostname'] = array('string'); 
 $meta['port'] = array('string', '_pattern' => '/^(\d+\|?)*$/'); 
